8211ddf3162d37baf3563cbe5e49682c81ae66e0
Syeed888 <SMSYEED.HOSSAIN@metropolia.fi>
2024-09-20 06:21:03 +0300

    Add files via upload

---
+++ Module2/Area of rectangle.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+parameter = (length + width) * 2
+area = length * width
+print("The parameter of the rectangle is: ", parameter)
+print("The area of the rectangle is: ", area)
