ef96da9189ae2a1c986fe175ba6c87fd0274f6b3
Syeed888 <SMSYEED.HOSSAIN@metropolia.fi>
2024-09-20 06:44:09 +0300

    Add files via upload

---
+++ Module2/Sum,product,average.py
@@ -0,0 +1,10 @@
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+number3 = float(input("Enter third number: "))
+
+sum = number1 + number2 + number3
+product = number1 * number2 * number3
+average = sum / 3
+print("The sum is: ",sum)
+print("The product is: ",product)
+print("The average is: ",average)
